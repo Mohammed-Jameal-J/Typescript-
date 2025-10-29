e90e1698fd8ddc46a75e38b474da931b3342ae76
Mohammed-Jameal-J <techprompt1031@gmail.com>
2025-10-29 15:53:50 +0530

    Types

---
+++ 1-types/1-types.js
@@ -0,0 +1,51 @@
+// primitive types
+// var userName = "Mohammed";
+// var userAge = 22;
+// var isStudent = false;
+// var empty = null;
+// var notDefined = undefined;
+// array 
+// var hobbies = ["reading", "traveling", "swimming"];
+// var num = [1, 2, 3, 4, 5];
+// tuples 
+// var person = ["Bob", 25];
+// special types ( any has no type checking, unknown has type checking )
+// var randomValue = 10;
+// randomValue = "Hello";
+// randomValue = true;
+// no error it works because no type checking
+// randomValue.toUpperCase();  
+// var inputValue = "World";
+// inputValue = 42;
+// inputValue = false;
+// to check its string or not 
+// if (typeof inputValue === "string") {
+//     inputValue.toUpperCase();  
+// }
+// void = it wont return anything  
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// function multiple(num1, num2) {
+//     return num1 * num2;
+// }
+// same like void we have never this will never return anything for long running process or error
+// function generateError(message) {
+//     throw new Error(message);
+// }
+// var address = {
+//     street: "St nagar",
+//     city: "Coimbatore",
+//     zipCode: 640001
+// };
+// console.log("Name: ".concat(userName));
+// console.log("Age: ".concat(userAge));
+// console.log("Is Student: ".concat(isStudent));
+// console.log("Empty: ".concat(empty));
+// console.log("Not Defined: ".concat(notDefined));
+// console.log("Hobbies: ".concat(hobbies.join(", ")));
+// console.log("Numbers: ".concat(num.join(", ")));
+// console.log("Person: Name - ".concat(person[0], ", Age - ").concat(person[1]));
+// add(5, 10);
+// console.log("Multiplication: ".concat(multiple(5, 10)));
+// console.log("Address: ".concat(address.street, ", ").concat(address.city, ", ").concat(address.zipCode));
---
+++ 1-types/1-types.ts
@@ -0,0 +1,64 @@
+// primitive types
+
+let userName: string = "Mohammed";
+let userAge: number = 22;
+let isStudent: boolean = false;
+let empty: null = null;
+let notDefined: undefined = undefined;
+
+// array 
+
+let hobbies: string[] = ["reading", "traveling", "swimming"];
+let num : number[] = [1, 2, 3, 4, 5];
+
+// tuples 
+let person: [string, number] = ["Bob", 25];
+
+// special types ( any has no type checking, unknown has type checking )
+
+let randomValue: any = 10;
+randomValue = "Hello";
+randomValue = true;
+// no error it works because no type checking
+// randomValue.toUpperCase();  
+
+let inputValue: unknown = "World";
+inputValue = 42;
+inputValue = false;
+
+// to check its string or not 
+// if (typeof inputValue === "string") {
+//     inputValue.toUpperCase();  
+// }
+
+
+// void = it wont return anything  
+function add(num1: number, num2: number): void {  
+    console.log(num1 + num2);
+}
+
+function multiple(num1: number, num2: number): number {  
+   return num1 * num2
+}
+// same like void we have never this will never return anything for long running process or error
+function generateError(message: string): never {
+    throw new Error(message);
+}
+
+let address = {
+    street: "St nagar",
+    city: "Coimbatore",
+    zipCode: 640001
+};
+
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+console.log(`Is Student: ${isStudent}`);
+console.log(`Empty: ${empty}`);
+console.log(`Not Defined: ${notDefined}`);
+console.log(`Hobbies: ${hobbies.join(", ")}`);
+console.log(`Numbers: ${num.join(", ")}`);
+console.log(`Person: Name - ${person[0]}, Age - ${person[1]}`);
+add(5, 10);
+console.log(`Multiplication: ${multiple(5, 10)}`);
+console.log(`Address: ${address.street}, ${address.city}, ${address.zipCode}`);
